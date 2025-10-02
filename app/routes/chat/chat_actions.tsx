9cedf6728674497fea136117569884b6245e74ea
raudikon <raudikon@gmail.com>
2025-10-02 15:00:42 -0400

    added system prompt to generate EOD

--- app/routes/chat/chat_actions.tsx
+++ app/routes/chat/chat_actions.tsx
@@ -5,22 +5,7 @@ import type { UIMessage } from 'ai';
 import { server_auth } from '../../server/auth.server';
 import { redirect } from 'react-router';
 
-
-// export async function loader({ request }: LoaderFunctionArgs) {
-//     console.log("Running loader")
-//     const session = await server_auth.api.getSession({
-//         headers: request.headers
-//     })
-
-//     if (session?.user) {
-//         console.log("Chat actions doesn't see no user!")
-//         return { user: session.user }
-//     }
-//     else {
-//         throw redirect('/login')
-//     }
-// }
-
+import { eod_prompt } from './eod_prompt';
 /*
 * Scout's Notes 
 - Asynchronous function that makes a POST request, accepting 1 argument. The argument is of type Request and that comes from fetch API 
@@ -28,13 +13,16 @@ import { redirect } from 'react-router';
 - convertToModelMessages converts UIMessage to ModelMessage objects, why are we doing that. 
 */
 export const maxDuration = 30; //allow streaming responses up to 30s
+
 export async function action({ request }: ActionFunctionArgs) {
 
+    console.log("calling chat actions")
     const { messages }: { messages: UIMessage[] } = await request.json();
 
     const result = streamText({
         model: openai('gpt-4o'),
         messages: convertToModelMessages(messages),
+        system: eod_prompt
     });
 
     return result.toUIMessageStreamResponse();
